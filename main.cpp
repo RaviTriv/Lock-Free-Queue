8ede6376c5487255e7b2962f8ec5d24879e58f9e
Ravi <trivediravi561@gmail.com>
2024-11-03 09:20:52 -0500

    super basic regular queue

--- main.cpp
+++ main.cpp
@@ -12,10 +12,57 @@ public:
   }
 };
 
+class Queue
+{
+public:
+  Node *head;
+  Node *tail;
+  Queue(Node *head, Node *tail)
+  {
+    this->head = head;
+    this->tail = tail;
+  }
+  void enqueue(int val)
+  {
+    // check if head is nullptr
+    if (head == nullptr)
+    {
+      head = new Node(val, nullptr);
+    }
+    else if (head->next == nullptr)
+    {
+      // check if tail is nullptr
+      tail = new Node(val, nullptr);
+      head->next = tail;
+    }
+    else
+    {
+      Node *temp = new Node(val, nullptr);
+      tail->next = temp;
+      tail = temp;
+    }
+  }
+
+  int dequeue()
+  {
+    int val = head->data;
+    Node *temp = head->next;
+    head = temp;
+    return val;
+  }
+};
+
 int main()
 {
-  Node *f = new Node(21, nullptr);
-  Node *s = new Node(22, nullptr);
-  f->next = s;
-  printf("NEXT NODE VALUE: %d\n", f->next->data);
+  Queue *q = new Queue(nullptr, nullptr);
+  q->enqueue(21);
+  q->enqueue(42);
+  q->enqueue(63);
+  q->enqueue(70);
+  q->enqueue(22);
+  printf("%d\n", q->dequeue());
+  printf("%d\n", q->dequeue());
+  printf("%d\n", q->dequeue());
+  printf("%d\n", q->dequeue());
+  printf("%d\n", q->dequeue());
 }
