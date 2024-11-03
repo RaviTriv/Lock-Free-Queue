f772a234253f794a6cf4503c1f8fe4b21f57862b
Ravi <trivediravi561@gmail.com>
2024-11-03 13:53:38 -0500

    implementing thread safe queue

--- README.md
+++ README.md
@@ -4,4 +4,52 @@ Simple lock free queue to reduce latency.
 
 Typically a lock is used for synchronization in data structures, this can be a bottleneck for parallel programs.
 
+Synchorinzation is necessary for thread safe queues.
+A simple example that uses a non thread safe queue can illustrate this.
+
+```
+void qHelper(Queue *q)
+{
+  q->enqueue(21);
+}
+
+for(int i = 0; i < 1000000; i++){
+  thread t1(qHelper);
+  t1.detach();
+}
+printf("QUEUE SIZE: %d\n", q->size);
+```
+
+Running the program three times the following is outputted.
+
+```
+QUEUE SIZE: 999876
+QUEUE SIZE: 999913
+QUEUE SIZE: 999897
+```
+
+The expectation is that our ouput is 1000000.
+
+Our threads are sometimes writing at the same time overriding one another. For example `thread x` creates a `new node a` and `thread y` creates a `new node b`. Before `thread x` can complete its operation of updating the pointing the `tail's next value` to `node a` and setting `node a` as the new tail, `thread y` comes in and sets the current `tail's next value` to `node b`. We should be waiting for `node a` to become the tail value before setting the `tail's next value to node b`.
+
+A simple change to our queue can ensure thread safety in our `enqueue` operation.
+
+```
+mutex m;
+void enqueue(int val){
+  m.lock()
+  //Operation to add new element to our queue
+  m.unlock()
+}
+```
+This pretty much allows us to ensure only 1 thread can execute the critical section at a time.
+The updated code outputs the following.
+```
+QUEUE SIZE: 1000000
+QUEUE SIZE: 1000000
+QUEUE SIZE: 1000000
+```
+
+The example above is a blocking data structure. This can result in increased latency as a result of the thread switching that occurs when a thread is blocked.
+
 When we want to write an element we use a lock preventing other threads from gaining access. An issue that can arise with this is if our thread gets suspended and we don't release the lock, the other thread will still be stuck waiting.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <cstdio>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
+
 using namespace std;
 class Node
 {
@@ -18,6 +21,8 @@ class Queue
 public:
   Node *head;
   Node *tail;
+  mutex m;
+  int size = 0;
   Queue(Node *head, Node *tail)
   {
     this->head = head;
@@ -25,6 +30,7 @@ public:
   }
   void enqueue(int val)
   {
+   // m.lock();
     // check if head is nullptr
     if (head == nullptr)
     {
@@ -42,84 +48,35 @@ public:
       tail->next = temp;
       tail = temp;
     }
+    size++;
+    //m.unlock();
   }
 
   int dequeue()
   {
+    m.lock();
     int val = head->data;
     Node *temp = head->next;
     head = temp;
+    m.unlock();
+    size--;
     return val;
   }
 };
 
-void doQueueStuff(Queue *q, int threadNumber)
+void doQueueStuff(Queue *q)
 {
-  for (int i = 0; i < 13; i++)
-  {
-    printf("ADDING FROM THREAD: %d\n", threadNumber);
-    q->enqueue(21);
-    printf("ADDING FROM THREAD: %d\n", threadNumber);
-    q->enqueue(42);
-    printf("ADDING FROM THREAD: %d\n", threadNumber);
-    q->enqueue(63);
-  }
+  q->enqueue(21);
 }
 
 int main()
 {
   Queue *q = new Queue(nullptr, nullptr);
-
-  thread t1(doQueueStuff, q, 1);
-  thread t2(doQueueStuff, q, 2);
-  thread t3(doQueueStuff, q, 3);
-  thread t4(doQueueStuff, q, 4);
-  thread t5(doQueueStuff, q, 5);
-  thread t6(doQueueStuff, q, 6);
-  thread t7(doQueueStuff, q, 7);
-  thread t8(doQueueStuff, q, 8);
-  thread t9(doQueueStuff, q, 9);
-  thread t10(doQueueStuff, q, 10);
-  thread t11(doQueueStuff, q, 11);
-  thread t12(doQueueStuff, q, 12);
-  thread t13(doQueueStuff, q, 13);
-  thread t14(doQueueStuff, q, 14);
-  thread t15(doQueueStuff, q, 15);
-  thread t16(doQueueStuff, q, 16);
-  // printf("ADDING FROM MAIN\n");
-  // q->enqueue(21);
-  // printf("ADDING FROM MAIN\n");
-  // q->enqueue(42);
-  // printf("ADDING FROM MAIN\n");
-  // q->enqueue(63);
-  // printf("ADDING FROM MAIN\n");
-  // q->enqueue(70);
-  // printf("ADDING FROM MAIN\n");
-  // q->enqueue(22);
-  t1.join();
-  t2.join();
-  t3.join();
-  t4.join();
-  t5.join();
-  t6.join();
-  t7.join();
-  t8.join();
-  t9.join();
-  t10.join();
-  t11.join();
-  t12.join();
-  t13.join();
-  t14.join();
-  t15.join();
-  t16.join();
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
-  // printf("%d\n", q->dequeue());
+  for (int i = 0; i < 1000000; i++)
+  {
+    thread t1(doQueueStuff, q);
+    t1.detach();
+  }
+  
+  printf("QUEUE SIZE: %d\n", q->size);
 }
